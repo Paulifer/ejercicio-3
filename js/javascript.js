171b5da563bddbd15bc3ebb38420e8557d6e6d7d
paulina huañaco <pauli.hu.timm@gmail.com>
2017-04-24 16:35:24 -0300

    terminado el ejercicio 3 de html

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function convertirCelsius() {
+    
+var celsius = document.getElementById('dato').value; 
+var calculo = celsius * 9 / 5 + 32;  
+   alert("tu resultado en grados Fahrenheit es " + calculo);
+}
